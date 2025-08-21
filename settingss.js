e772702719f23382a4f876a961b41a918fcc3ccc
dripper-gif <juniormuinde5@gmail.com>
2025-08-21 15:15:50 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,40 @@
+name: Node.js CI
 
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [20.x]
+
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v3
+
+    - name: Set up Node.js
+      uses: actions/setup-node@v3
+      with:
+        node-version: ${{ matrix.node-version }}
+
+    - name: Install dependencies
+      run: npm install
+
+    - name: Start application
+      run: npm start
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~EvFwmJjK#s7OCL8dL3y9xSZ6Ull7miSvUsEqJwIUwiu6hQyiskBU",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254768890007", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
