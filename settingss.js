3d192388d0bb38039d1cf6b8e7c0d098b65e6bbf
dripper-gif <juniormuinde5@gmail.com>
2025-08-21 14:58:21 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~EvFwmJjK#s7OCL8dL3y9xSZ6Ull7miSvUsEqJwIUwiu6hQyiskBU",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "254768890007, // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
